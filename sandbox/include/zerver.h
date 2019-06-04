03f827f3a4c83752e48e719bc23b4787ac265c74
Zach Schuermann <zachary.zvs@gmail.com>
2019-06-04 10:49:44 -0500

    Working

---
+++ sandbox/include/zerver.h
@@ -0,0 +1,43 @@
+#ifndef __ZERVER_H_
+#define __ZERVER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include "util.h"
+
+#define SERVER_STRING "Server: zerver/0.1.0\r\n"
+
+void fatal(const char*);
+int setup_listen_socket(int port);
+void server_loop(int server_socket);
+void handle_client(int client_socket);
+void handle_http_method(char* method_buffer, int client_socket);
+void handle_get_method(char* path, int client_socket);
+void handle_post_method(char* path, int client_socket);
+void handle_unimplemented_method(int client_socket);
+void handle_http_404(int client_socket);
+void send_headers(const char* path, off_t len, int client_socket);
+void transfer_file_contents(char* file_path, int client_socket, off_t file_size);
+
+const int DEFAULT_SERVER_PORT = 5051;
+const char* http_404_content = \
+        "HTTP/1.0 404 Not Found\r\n"
+        "Content-type: text/html\r\n"
+        "\r\n"
+        "<html>"
+        "<head>"
+        "<title>Zerver: Not Found</title>"
+        "</head>"
+        "<body>"
+        "<h1>Not Found (404)</h1>"
+        "<p>Client request for an object that was not found on this server.</p>"
+        "</body>"
+        "</html>";
+
+#endif // __ZERVER_H_
--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -1,28 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <sys/sendfile.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include "util.h"
-
-#define SERVER_STRING "Server: zerver/0.1\r\n"
-
-void fatal(const char*);
-int setup_listen_socket(int port);
-void server_loop(int server_socket);
-void handle_client(int client_socket);
-void handle_http_method(char* method_buffer, int client_socket);
-void handle_get_method(char* path, int client_socket);
-void handle_post_method(char* path, int client_socket);
-void handle_unimplemented_method(int client_socket);
-void handle_http_404(int client_socket);
-void send_headers(const char* path, off_t len, int client_socket);
-void transfer_file_contents(char* file_path, int client_socket, off_t file_size);
-
-const int DEFAULT_SERVER_PORT = 5051;
+#include "zerver.h"
 
 void fatal(const char* message){
    fprintf(stderr, "%s", message);
@@ -133,7 +109,7 @@ void handle_http_method(char* method_buffer, int client_socket){
 
 
 void handle_http_404(int client_socket) {
-   return;
+   send(client_socket, http_404_content, strlen(http_404_content), 0);
 }
 
 void handle_get_method(char* path, int client_socket) {
