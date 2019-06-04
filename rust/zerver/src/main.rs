e8f0326f1451180c54a193e77467c8e4b468d5b6
Zach Schuermann <zachary.zvs@gmail.com>
2019-06-04 18:48:42 -0500

    rust mostly working - need to debug http resp

--- rust/zerver/src/main.rs
+++ rust/zerver/src/main.rs
@@ -1,45 +1,111 @@
-//extern crate nix;
-use std::thread;
-use std::os::unix::net::{UnixStream, UnixListener};
+use std::net::{TcpListener, TcpStream};
+use std::fs::File;
+use std::io::BufWriter;
+use std::io::prelude::*;
 
 const DEFAULT_SERVER_PORT: i16 = 5051;
 
-fn handle_client(stream: UnixStream) {
-    println!("Got request!")
+fn send_headers(stream: &mut TcpStream, path: String) {
+    println!("sending header");
+    let mut writer = BufWriter::new(stream);
+    let header = "HTTP/1.0 200 OK\r\n\
+                  Content-Type: text/html\r\n\
+                  content-length: 5000\r\n\
+                  \r\n";
+    writer.write_all(header.as_bytes()).unwrap();
 }
 
-fn setup_listen_socket(port: i16) {
-    //sock;
-    //struct sockaddr_in server_addr;
+//fn send_file_contents() {
+//    println!("Sending file contents");
+//    let mut writer = BufWriter::new(stream);
+//    writer.write_all(resp404.as_bytes()).unwrap();
+//}
 
-    let listener = UnixListener::bind("localhost:5051").unwrap();
+fn handle_http_404(stream: &mut TcpStream) {
+    let resp404 = "HTTP/1.0 404 Not Found\r
+        Content-type: text/html\r
+        \r
+        <html>
+        <head>
+        <title>Zerver: Not Found</title>
+        </head>
+        <body>
+        <h1>Not Found (404)</h1>
+        <p>Client request for an object that was not found on this server.</p>
+        </body>
+        </html>";
 
-    match listener.accept() {
-        Ok((socket, addr)) => println!("Got a client: {:?}", addr),
-        Err(e) => println!("accept function failed: {:?}", e),
+    let mut writer = BufWriter::new(stream);
+    writer.write_all(resp404.as_bytes()).unwrap();
+}
+
+fn handle_get_method(stream: &mut TcpStream, path: &str) {
+    let mut p = String::from("public/");
+    // switch on last char to see if client is asking for dir
+    if path.chars().last().unwrap() == '/' {
+        p.push_str(path);
+        p.push_str("index.html")
+    } else {
+        p.push_str(path);
     }
 
-    // accept connections and process them, spawning a new thread for each one
-    //for stream in listener.incoming() {
-    //    match stream {
-    //        Ok(stream) => {
-    //            /* connection succeeded */
-    //            thread::spawn(|| handle_client(stream));
-    //        }
-    //        Err(err) => {
-    //            /* connection failed */
-    //            break;
-    //        }
-    //    }
-    //}
+    let f = File::open(p);
+    if f.is_err() {
+        handle_http_404(stream);
+    }
 
-    // create TCP socket
-    // socket(AF_INTET, socket_type, protocol) in C
-    //sock = nix::sys::socket::socket<>();
+    let mut buffer = Vec::new();
+    // read the whole file
+    f.unwrap().read_to_end(&mut buffer);
+
+    //println!("{:?}", f);
+    println!("Sending file contents");
+    let mut writer = BufWriter::new(stream);
+    writer.write_all(&buffer).unwrap();
 }
 
-fn main() {
-    let server_port = DEFAULT_SERVER_PORT;
-    let server_socket = setup_listen_socket(server_port);
-    println!("Running zerverd on port 5051");
+fn handle_http_method(stream: &mut TcpStream, ln: String) {
+    let split = ln.split(" ");
+    let vec: Vec<&str> = split.collect();
+
+    let method = vec[0];
+    let path = vec[1];
+    // protocol NYI
+    let _protocol = vec[2];
+    if method != "GET" {
+        eprintln!("Unsupported HTTP method");
+    }
+    handle_get_method(stream, path);
+}
+
+fn handle_client(stream: &mut TcpStream) {
+    println!("Got request!");
+
+    // why does this have to be mutable?
+    // read the whole file -- fails (hangs) due to not terminating somehow?
+    //let mut buffer = Vec::new();
+    //stream.read_to_end(&mut buffer).expect("Failed to read TcpStream to buf");
+    //or we can do fixed size
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer);
+    for line in buffer.lines() {
+        println!("{:?}", line); //these are all surrounded in Ok()
+        let ln = line.unwrap();
+        println!("{}", ln);
+        handle_http_method(stream, ln);
+        break; // only using first line
+    }
+    //println!("{:?}", buffer);
+}
+
+fn main() -> std::io::Result<()> {
+
+    let host = format!("127.0.0.1:{}", DEFAULT_SERVER_PORT);
+    let listener = TcpListener::bind(host)?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(&mut stream?);
+    }
+    Ok(())
 }
